54c9ad27001f7b7317f66b6831823f7923be5532
alamin-cse-20 <alamin.sust2020@gmail.com>
2025-08-04 17:31:15 +0600

    solved problem 6 in o(logn) in stead of o(n)

--- problem_06.js
+++ problem_06.js
@@ -2,8 +2,14 @@
 var experience = 30;
 var startingSalary = 45000;
 //write your code here
-var presentSalary = startingSalary;
-for (var i = 1; i<=experience; i++) {
-    presentSalary += 0.05 * presentSalary;
-}
+
+// var presentSalary = startingSalary;
+// for (var i = 1; i<=experience; i++) {
+//     presentSalary += 0.05 * presentSalary;
+// }
+// console.log(presentSalary.toFixed(2));
+
+
+// Using compound interest formula
+var presentSalary = startingSalary * Math.pow(1.05, experience);
 console.log(presentSalary.toFixed(2));
